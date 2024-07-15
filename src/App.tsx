f360a496de0aba53263eccc08ae4441f282acd54
Daria Liferova <liferova.daria@gmail.com>
2024-07-15 13:13:33 +0200

    feat: prepare project tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import "./App.css"
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-4xl uppercase bold">Your personalized coffee</h1>
+      <p className="text-4xl font-bold">Your personalized coffee</p>
+      <p className="text-4xl font-semibold">Your personalized coffee</p>
+      <p className="text-4xl">Your personalized coffee</p>
+      <p className="text-4xl italic">Your personalized coffee</p>
+
+      <h2 className="font-montserrat text-2xl font-semibold uppercase">
+        Coffee build your base
+      </h2>
+      <p className="font-montserrat text-2xl font-black">
+        Coffee build your base
+      </p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "30rem",
+        md: "48rem",
+        lg: "61rem",
+        xl: "90rem",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        sm: ["0.875rem", "0.875rem"],
+        base: ["1rem", "1.5"],
+        xl: ["1.25rem", "1.2"],
+        "2xl": ["1.5rem", "1.75rem"],
+        "3xl": ["1.875rem", "2.125rem"],
+        "4xl": ["3.375rem", "4.375rem"],
+      },
+      fontWeight: {
+        black: 900,
+      },
+      colors: {
+        "primary-color": "#415167",
+        "accent-color": "#C7A17A",
+        "accent-color-lighter": "#F9F5E8",
+        "dark-grey": "#151D28",
+        "light-gray": "#EDF0F5",
+      },
+    },
   },
   plugins: [],
 }
-
